dc5faf26d079026e063b647c86d1794c9109f31b
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 20:00:20 +0800

    toilet transition added

--- scripts/toilet.gd
+++ scripts/toilet.gd
@@ -1,8 +1,18 @@
 extends Control
 
+@onready var transition_player = $Transition/Transitions/AnimationPlayer
+@onready var overlay = $Transition/Transitions/ColorRect
+
+# The lock to prevent clicking while transitioning
+var is_transitioning = false
+
 func _ready():
 	$door.pressed.connect(_on_door_pressed)
 	$Inventory.current_room = "toilet"
+	transition_player.play("black_to_fade")
+	overlay.mouse_filter = Control.MOUSE_FILTER_STOP
+	await transition_player.animation_finished
+	overlay.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 	for item in $Item.get_children():
 		item.add_to_group("items")
@@ -10,4 +20,6 @@ func _ready():
 			item.visible = false
 
 func _on_door_pressed():
+	if is_transitioning:
+		return
 	get_tree().change_scene_to_file("res://scenes/corridor.tscn")
