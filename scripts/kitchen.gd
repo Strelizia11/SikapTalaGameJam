479623e487c1b40e122ed040697cf8497e978bf3
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 10:00:56 +0800

    added kitchen door to corridor script

--- scripts/kitchen.gd
+++ scripts/kitchen.gd
@@ -1,4 +1,7 @@
 extends Control
 
-func _on_door_to_corridor_pressed():
+func _ready():
+	$DoortoCorridor.pressed.connect(_on_corridor_door_pressed)
+
+func _on_corridor_door_pressed():
 	get_tree().change_scene_to_file("res://scenes/corridor.tscn")
