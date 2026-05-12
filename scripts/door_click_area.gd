433001c278da1ef1b6ae41a1ffa995d9fc575880
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 13:59:23 +0800

    Fixed corridor kitchen door hitbox

--- scripts/door_click_area.gd
+++ scripts/door_click_area.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("Door clicked!")
-			on_door_pressed()
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		# This checks if the mouse is actually over your polygon
+		# 'get_node("KitchenDoorEntry")' is your CollisionPolygon2D
+		var poly = $KitchenDoorEntry 
+		if Geometry2D.is_point_in_polygon(get_local_mouse_position(), poly.polygon):
+			print("Kitchen door clicked (via global input)!")
+			enter_kitchen()
 
-func on_door_pressed():
-	get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
+func enter_kitchen():
+	print("Attempting to switch...")
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/kitchen.tscn")
