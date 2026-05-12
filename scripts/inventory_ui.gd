25c1ad19244303ad6ceb264b00bd2470b70f80fb
Aeron Supremido <supremidoaa.923.stud@udd.edu.ph>
2026-05-12 19:22:32 +0800

    fix the inventory global and the slot (fix the slot image size)

--- scripts/inventory_ui.gd
+++ scripts/inventory_ui.gd
@@ -6,10 +6,47 @@ extends CanvasLayer
 	$InventoryBar/Slot3
 ]
 
+var item_textures = {
+	"hankerchief": preload("res://assets/sprites/items/hankerchief.jpg"),
+	"medicine":    preload("res://assets/sprites/items/medicine.jpg"),
+	"mirror":      preload("res://assets/sprites/items/mirror.jpg"),
+}
+
+var slot_empty_texture = preload("res://assets/sprites/inventory-slot.png")
+var current_room: String = ""
+
 func _ready():
+	add_to_group("inventory_ui")
 	refresh()
+	for i in range(slots.size()):
+		slots[i].gui_input.connect(_on_slot_clicked.bind(i))
 
 func refresh():
 	for i in range(slots.size()):
-		var item = InventoryManager.slots[i]
-		# future: swap texture or show item icon here
+		var item = InventoryManager.slots[i]["item"]
+		if item != "":
+			slots[i].texture = item_textures.get(item, slot_empty_texture)
+		else:
+			slots[i].texture = slot_empty_texture
+
+func _on_slot_clicked(event: InputEvent, slot_index: int):
+	if not (event is InputEventMouseButton):
+		return
+	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var item_name = InventoryManager.slots[slot_index]["item"]
+		var item_room  = InventoryManager.slots[slot_index]["room"]
+
+		if item_name == "":
+			return
+
+		if current_room == item_room:
+			InventoryManager.remove_item(item_name)
+			refresh()
+			# Find the item node and restore it to its ORIGINAL spawn position
+			var item_node = get_tree().current_scene.find_child(item_name, true, false)
+			if item_node:
+				item_node.global_position = InventoryManager.get_spawn_position(item_name)
+				item_node.visible = true
+			print("Returned ", item_name, " to its original spot")
+		else:
+			print("Can't return here! ", item_name, " belongs in: ", item_room)
--- scripts/toilet.gd
+++ scripts/toilet.gd
@@ -2,6 +2,12 @@ extends Control
 
 func _ready():
 	$door.pressed.connect(_on_door_pressed)
+	$Inventory.current_room = "toilet"
+
+	for item in $Item.get_children():
+		item.add_to_group("items")
+		if InventoryManager.is_picked_up(item.item_name, "toilet"):
+			item.visible = false
 
 func _on_door_pressed():
 	get_tree().change_scene_to_file("res://scenes/corridor.tscn")
