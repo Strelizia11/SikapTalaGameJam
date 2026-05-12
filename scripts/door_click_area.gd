283fdcfe45d761337dcc619b52697382e3cf5894
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 15:12:17 +0800

    removed print in script for door clicks in corridor

--- scripts/door_click_area.gd
+++ scripts/door_click_area.gd
@@ -7,16 +7,12 @@ func _input(event):
 		var poly1 = $KitchenDoorEntry 
 		var poly2 = $ToiletDoorEntry 
 		if Geometry2D.is_point_in_polygon(get_local_mouse_position(), poly1.polygon):
-			print("Kitchen door clicked (via global input)!")
 			enter_kitchen()
 		elif Geometry2D.is_point_in_polygon(get_local_mouse_position(), poly2.polygon):
-			print("Toilet door clicked (via global input)!")
 			enter_toilet()
 
 func enter_kitchen():
-	print("Attempting to switch...")
 	get_tree().call_deferred("change_scene_to_file", "res://scenes/kitchen.tscn")
 	
 func enter_toilet():
-	print("Attempting to switch...")
 	get_tree().call_deferred("change_scene_to_file", "res://scenes/toilet.tscn")
