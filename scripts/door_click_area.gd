8baccf5a8505ba4e85b33c7a4e3a3bf6a0bcaa46
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 16:48:47 +0800

    fixed transition error

--- scripts/door_click_area.gd
+++ scripts/door_click_area.gd
@@ -1,31 +1,38 @@
 extends Area2D
 
-# Make sure this path matches your AnimationPlayer's location
 @onready var transition_player = get_node("../Transition/Transitions/AnimationPlayer")
+@onready var overlay = get_node("../Transition/Transitions/ColorRect")
+
+# This variable prevents clicking while an animation is running
+var is_transitioning = false
 
 func _input(event):
+	# If we are already transitioning, ignore all inputs!
+	if is_transitioning:
+		return
+		
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var poly1 = $KitchenDoorEntry
 		var poly2 = $ToiletDoorEntry
 		
 		if Geometry2D.is_point_in_polygon(get_local_mouse_position(), poly1.polygon):
-			enter_kitchen()
+			enter_room("res://scenes/kitchen.tscn")
 		elif Geometry2D.is_point_in_polygon(get_local_mouse_position(), poly2.polygon):
-			enter_toilet()
+			enter_room("res://scenes/toilet.tscn")
 
-func enter_kitchen():
-	# 1. Play the fade out
-	transition_player.play("fade_to_black")
+func enter_room(scene_path):
+	# 1. Set the lock to TRUE
+	is_transitioning = true
 	
-	# 2. Wait for the animation to finish
-	await transition_player.animation_finished
+	# 2. Block mouse clicks via the overlay
+	overlay.mouse_filter = Control.MOUSE_FILTER_STOP
 	
-	# 3. Now change the scene
-	get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
-	transition_player.play("black_to_fade")
-	
-func enter_toilet():
+	# 3. Play the fade out
 	transition_player.play("fade_to_black")
 	await transition_player.animation_finished
-	get_tree().change_scene_to_file("res://scenes/toilet.tscn")
-	transition_player.play("black_to_fade")
+	
+	# 4. Change the scene
+	get_tree().change_scene_to_file(scene_path)
+	
+	# 5. Note: Code after change_scene might not run if this node isn't an Autoload.
+	# If it blinks, you must move the transition to the Autoload settings.
--- scripts/kitchen.gd
+++ scripts/kitchen.gd
@@ -1,13 +1,37 @@
 extends Control
 
 @onready var transition_player = $Transition/Transitions/AnimationPlayer
+@onready var overlay = $Transition/Transitions/ColorRect
+
+# The lock to prevent clicking while transitioning
+var is_transitioning = false
 
 func _ready():
+	# When entering the kitchen, immediately fade in from black
 	transition_player.play("black_to_fade")
+	overlay.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	$DoortoCorridor.pressed.connect(_on_corridor_door_pressed)
 
 func _on_corridor_door_pressed():
-	get_tree().change_scene_to_file("res://scenes/corridor.tscn")
+	# If we are already mid-fade, don't do anything
+	if is_transitioning:
+		return
+		
+	# Start the transition process
+	enter_corridor()
 
-func fade_out():
-	transition_player.play("black_to_fade")
+func enter_corridor():
+	is_transitioning = true
+	
+	# 1. Block the mouse so they can't click again
+	overlay.mouse_filter = Control.MOUSE_FILTER_STOP
+	
+	# 2. Play the fade to black (Ensure you have a "fade_to_black" animation)
+	# If your animation is named differently, change this string:
+	transition_player.play_backwards("black_to_fade") 
+	
+	# 3. Wait for it to finish
+	await transition_player.animation_finished
+	
+	# 4. Change the scene
+	get_tree().change_scene_to_file("res://scenes/corridor.tscn")
