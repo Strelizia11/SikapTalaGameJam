f094238279759fd021ad8044164fdf106abefb04
Christian Aldana <rainealdana147@gmail.com>
2026-05-12 11:22:47 +0800

    changed main menu background

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 func _ready():
 	# Connect the buttons to their functions
-	$Menu/VBoxContainer/Button2.pressed.connect(_on_start_pressed)
-	$Menu/VBoxContainer/Button.pressed.connect(_on_quit_pressed)
+	$Menu/Button2.pressed.connect(_on_start_pressed)
+	$Menu/Button.pressed.connect(_on_quit_pressed)
 	# Optional: connect Credits button too
-	$Menu/VBoxContainer/Button3.pressed.connect(_on_credits_pressed)
+	$Menu/Button3.pressed.connect(_on_credits_pressed)
 
 func _on_start_pressed():
 	# Change this path to your actual game scene
