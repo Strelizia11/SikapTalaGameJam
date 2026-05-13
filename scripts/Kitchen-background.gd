ae61d2a7087c87e1576424a4683ce77900131e1a
Aeron Supremido <supremidoaa.923.stud@udd.edu.ph>
2026-05-13 11:26:39 +0800

    fix the kitchen door

--- scripts/Kitchen-background.gd
+++ scripts/Kitchen-background.gd
@@ -1,11 +1,9 @@
 extends Sprite2D
-
+ 
 # Drag and drop your background images into the Inspector array
 @export var background_textures: Array[Texture2D]
-
+ 
 func _ready():
-	var path = GlobalBackground.current_bg_path2
-	var music = GlobalBackground.bgm_path
+	var path = GlobalBackground.current_bg_path1
 	texture = load(path)
-	if has_node("/root/AudioManager"):  # ✅ safe check
-		AudioManager.play_bgm(load(music))
+ 
